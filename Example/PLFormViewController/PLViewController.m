cd2cd0184274b897783817a520c7cd68354d8990
Ash Thwaites <ash@toolbox-design.co.uk>
2015-11-18 13:12:47 +0000

    start the example form

--- Example/PLFormViewController/PLViewController.m
+++ Example/PLFormViewController/PLViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "PLViewController.h"
+@import PLForm;
 
-@interface PLViewController ()
+@interface PLViewController () <PLFormElementDelegate>
 
 @end
 
@@ -17,7 +18,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // lets add a simple form with a bunch of components
+    // create the form
+    PLConditionPresent *present = [[PLConditionPresent alloc] initWithLocalizedViolationString:NSLocalizedString(@"Please complete all fields", @"Please complete all fields")];
+    PLFormTextFieldElement *nameElement = [PLFormTextFieldElement textInputElementWithID:0 placeholderText:@"Event name" value:nil delegate:self];
+    PLFormDateFieldElement *dateElement = [PLFormDateFieldElement datePickerElementWithID:1 title:@"Event date" date:nil datePickerMode:UIDatePickerModeDate delegate:self];
+    PLFormDateFieldElement *timeElement = [PLFormDateFieldElement datePickerElementWithID:2 title:@"Event time" date:nil datePickerMode:UIDatePickerModeTime delegate:self];
+    PLFormSelectFieldElement *repeatElement = [PLFormSelectFieldElement selectElementWithID:3 title:@"Repeat interval" values:@[@"Never",@"Weelky",@"Monthly"] index:0 delegate:self];
+    PLFormSelectFieldElement *repeatFormatElement = [PLFormSelectFieldElement selectElementWithID:4 title:@"Repeat format" values:@[@"Since first",@"Since last",@"Until next"] index:0 delegate:self];
+    PLFormSwitchFieldElement *weeksElemet = [PLFormSwitchFieldElement switchFieldElementWithID:5 title:@"Display weeks" value:NO delegate:self];
+    PLFormSwitchFieldElement *lightElemet = [PLFormSwitchFieldElement switchFieldElementWithID:7 title:@"Light theme" value:NO delegate:self];
+    
+    
+    PLMemoryDataSource *memDataSource = [PLMemoryDataSource new];
+    self.dataSource = memDataSource;
+    
+    [memDataSource addItems:@[nameElement,dateElement,timeElement,repeatElement]];
+    [memDataSource addItems:@[repeatFormatElement,weeksElemet,lightElemet] toSection:1];
+    
+//    DTSectionModel *mainSection = (DTSectionModel*)self.memoryStorage.sections[0];
+//    [mainSection setTableSectionHeader:@"Event details"];
+//    
+//    DTSectionModel *displaySection = (DTSectionModel*)self.memoryStorage.sections[1];
+//    [displaySection setTableSectionHeader:@"Display details"];
+
 }
 
 - (void)didReceiveMemoryWarning
